622f955860ce726395797bc21c9cc2de04f082af
scotthconner@gmail.com <scotthconner@gmail.com>
2022-09-28 11:38:58 -0500

    Notes: * Made the look and feel of the keys page better

--- src/Keys.js
+++ src/Keys.js
@@ -3,16 +3,31 @@
 //////////////////////////////////////
 import {
   Box, BoxProps,
+  Center,
   Heading,
+  HStack,
   Skeleton,
   Stack,
+  Tag,
+  TagLabel,
+  Text,
+  Wrap,
+  WrapItem,
+  VStack,
   useColorModeValue
 } from '@chakra-ui/react'
 
+import { FaTimes } from 'react-icons/fa';
+import { GiBossKey } from 'react-icons/gi';
+import { HiOutlineKey } from 'react-icons/hi';
 
 //////////////////////////////////////
 // Wallet, Network, Contracts
 //////////////////////////////////////
+import {
+  useAccount
+} from 'wagmi';
+
 import {
   useWalletKeys,
   useKeyInfo,
@@ -27,24 +42,31 @@ function Keys() {
 
   if (!keys.isSuccess) {
     keyBody = (<>
-      <Skeleton height='6em'/>
-      <Skeleton height='6em'/>
-      <Skeleton height='6em'/>
-    </>); 
+      <WrapItem key='1'>
+        <Box w='10em' h='12em'>
+          <Skeleton height='12em' borderRadius='lg'/>
+        </Box>
+      </WrapItem>
+      <WrapItem key='2'>
+        <Box w='10em' h='12em'>
+          <Skeleton height='12em' borderRadius='lg'/>
+        </Box>
+      </WrapItem>
+      <WrapItem key='3'>
+        <Box w='10em' h='12em'>
+          <Skeleton height='12em' borderRadius='lg'/>
+        </Box>
+      </WrapItem></>); 
   } else {
-      keyBody = (<>
-      {keys.data.map((k) => (
-        <Key keyId={k}/>
-      ))}
-    </>);
+    keyBody = (keys.data.map((k) => (<Key key={k} keyId={k}/>)));
   }
 
   return (
     <Stack m='1em' spacing='1em'>
       <Heading size='md'>Keys in Your Wallet</Heading>
-      <Stack spacing='1.5em'>
-        {keyBody}
-      </Stack>
+        <Wrap padding='3em' spacing='2em'>
+          {keyBody}
+        </Wrap>
     </Stack>
   );
 }
@@ -55,12 +77,45 @@ interface KeyProps extends BoxProps {
 
 const Key = ({keyId, ...rest}: KeyProps) => {
   var boxColor = useColorModeValue('white', 'gray.800');
-  var keyInfo = useKeyInfo(keyId);
+  var account = useAccount(); 
+  var keyInfo = useKeyInfo(keyId, account.address);
+  var body = '';
+
+  if(!keyInfo.isSuccess) {
+    body = (
+      <></>
+    );
+  } else {
+    body = (
+      <Center w='8em'>
+        <VStack spacing='1em'>
+          <Text fontSize='xs' fontStyle='italic' color='gray.500'>{keyInfo.data.trust.name}</Text>
+          <HStack>
+            {keyInfo.data.isRoot ? <GiBossKey size='56px'/> : <HiOutlineKey size='56px'/>}
+            <FaTimes/>
+            <Text fontSize='2xl'>{keyInfo.data.inventory.toString()}</Text>
+          </HStack>
+          <Text>{keyInfo.data.alias}</Text>
+          {keyInfo.data.soulbound.toString() != '0' && 
+            <Tag colorScheme='purple'><TagLabel>{keyInfo.data.soulbound.toString()} × Soulbound</TagLabel></Tag>}
+        </VStack>
+      </Center>
+    )
+  }
 
   return (
-    <Box p='1em' h='6em' borderRadius='lg' bg={boxColor} boxShadow='dark-lg'>
-      {keyId.toNumber()} 
-    </Box>
+    <WrapItem key={keyId} p='1em' w='10em' h='12em' 
+      border={keyInfo.isSuccess && keyInfo.data.isRoot ? '2px' : '0px'} 
+      borderColor={keyInfo.isSuccess && keyInfo.data.isRoot ? 'yellow.400' : 'white'}
+      borderRadius='lg' 
+      bg={boxColor} boxShadow='dark-lg'
+      cursor='pointer'
+      _hover= {{
+        transform: 'scale(1.1)',
+        transition: 'all 0.2s ease-in-out'
+      }}>
+      {body}
+    </WrapItem>
   );
 }
 
--- src/hooks/LocksmithHooks.js
+++ src/hooks/LocksmithHooks.js
@@ -2,6 +2,7 @@ import Locksmith from '../services/Locksmith.js';
 import {useState, useEffect} from 'react';
 import {useQuery} from 'react-query';
 import {useAccount, useProvider, useContract} from 'wagmi';
+import {ethers} from 'ethers';
 
 /**
  * useWalletKeys
@@ -29,17 +30,72 @@ export function useWalletKeys(address) {
 /**
  * useKeyInfo 
  *
- * This hook takes a KeyId, and calls Locksmith#inspectKey. 
+ * This hook takes a KeyId, and calls 
+ *  - Locksmith#inspectKey
+ *  - Locksmith#trustRegistry
  *
  * @param keyId the key you want to you inspect 
  * @return an query that returns the key Ids
  */
-export function useKeyInfo(keyId) {
+export function useKeyInfo(keyId, address = null) {
   const provider   = useProvider();
   const locksmith = useContract(Locksmith.getContract('locksmith', provider));
+  const keyVault = useContract(Locksmith.getContract('keyVault', provider));
   const keyInfo = useQuery('keyInfo for ' + keyId, async function() {
-    return await locksmith.inspectKey(keyId);
+    let trust = null;
+    let held = null;
+    let soulboundCount = null;
+
+    // call #inspectKey
+    let response = await locksmith.inspectKey(keyId);
+
+    // get the trust slug if the key is valid
+    if (response[0]) {
+      // grab the trust information
+      trust = await locksmith.trustRegistry(response[2]);
+
+      // get the user's inventory
+      if(address) {
+        held = await keyVault.balanceOf(address, keyId);
+      }
+
+      // determine how many of them are soulbound
+      soulboundCount = await keyVault.soulboundKeyAmounts(address, keyId); 
+    }
+
+    return {
+      isValid: response[0],
+      alias: ethers.utils.parseBytes32String(response[1]),
+      inventory: held,
+      soulbound: soulboundCount,
+      isRoot: response[3],
+      trust: {
+        id: response[2], 
+        name: ethers.utils.parseBytes32String(trust.name),
+        rootKeyId: trust.rootKeyId,
+        keys: response[4]
+      }
+    };
   });
 
   return keyInfo;
+}
+
+/**
+ * useTrustSlug
+ *
+ * Use this to look into the public trust registry.
+ *
+ * @param trustId the trust Id you want the slug for.
+ * @return the Trust struct from the contract.
+ **/
+export function useTrustSlug(trustId) {
+  const provider   = useProvider();
+  const locksmith = useContract(Locksmith.getContract('locksmith', provider));
+  const trustInfo = useQuery('trustSlug for ' + trustId, async function() {
+    let response = await locksmith.trustRegistry(trustId);
+    console.log(response);
+  });
+
+  return trustInfo;
 }
