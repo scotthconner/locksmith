7136765e6236fcb9bc79b888f501835a7caca7e2
scotthconner@gmail.com <scotthconner@gmail.com>
2022-11-01 22:02:36 -0500

    Notes: * Imported the alarm clock ABI from the deployment into Locksmith * Introduced a new dependency - 'react-datepicker' for choosing the date   of an alarm clock configuration. * Built out the initial pass of the modal dialog for choosing alarm   configuration. * Started building out the hooks for the Alarm clock, still missing   useSnoozeAlarm and useChallengeAlarm

---
+++ src/hooks/AlarmClockHooks.js
@@ -0,0 +1,51 @@
+import Locksmith from '../services/Locksmith.js';
+import {useQuery} from 'react-query';
+import {
+  useProvider,
+  useContract,
+  usePrepareContractWrite,
+  useContractWrite
+} from 'wagmi';
+import {ethers} from 'ethers';
+
+/**
+ * useAlarm
+ *
+ * Given an event hash, will grab the alarm metadata.
+ */
+export function useAlarm(eventHash) {
+  const provider = useProvider();
+  const alarmClock = useContract(Locksmith.getContract('alarmClock', provider));
+  return useQuery('alarms for ' + eventHash, async function() {
+    var response = await alarmClock.alarms(eventHash);
+    return {
+      eventHash: response[0],
+      alarmTime: response[1],
+      snoozeInterval: response[2],
+      snoozeKeyId: response[3]
+    };
+  });
+}
+
+/**
+ * useCreateAlarm 
+ *
+ * Will call #createAlarm on the contract.
+ */
+export function useCreateAlarm(rootKeyId, description, alarmTime, snoozeInterval, snoozeKeyId, errorFunc, successFunc) {
+  const preparation = usePrepareContractWrite(
+    Locksmith.getContractWrite('alarmClock', 'createAlarm',
+      [rootKeyId, ethers.utils.formatBytes32String(description||''), alarmTime, snoozeInterval, snoozeKeyId],
+      rootKeyId !== null && description.length > 3 && snoozeInterval !== null && snoozeKeyId !== null && alarmTime !== null
+    )
+  );
+
+  return useContractWrite({...preparation.config,
+    onError(error) {
+      errorFunc(error);
+    },
+    onSuccess(data) {
+      successFunc(data);
+    }
+  });
+}
--- src/trusts/Events.js
+++ src/trusts/Events.js
@@ -26,8 +26,11 @@ import {
   useToast,
 } from '@chakra-ui/react';
 import { useState } from 'react';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
 import { KeyInfoIcon } from '../components/KeyInfo.js';
-import { BiCheckCircle } from 'react-icons/bi';
+import { BiCheckCircle, BiAlarm } from 'react-icons/bi';
 import { HiOutlineLightningBolt } from 'react-icons/hi';
 import { FiPower } from 'react-icons/fi';
 import { IoIosHourglass } from 'react-icons/io';
@@ -45,6 +48,10 @@ import {
   useEventKey,
   useCreateKeyOracle,
 } from '../hooks/KeyOracleHooks.js';
+import {
+  useAlarm,
+  useCreateAlarm,
+} from '../hooks/AlarmClockHooks.js';
 
 export function TrustEvent({trustId, eventHash, ...rest}) {
 	var boxColor = useColorModeValue('white', 'gray.800');
@@ -52,9 +59,11 @@ export function TrustEvent({trustId, eventHash, ...rest}) {
     const eventState = useEventState(eventHash);
     const eventDispatcher = useEventDispatcher(eventHash);
     const keyOracleAddress = Locksmith.getContractAddress('keyOracle');
+    const alarmClockAddress = Locksmith.getContractAddress('alarmClock');
 
     const eventTypes = {}
     eventTypes[keyOracleAddress] = 'KEY_ORACLE';
+    eventTypes[alarmClockAddress] = 'ALARM_CLOCK';
     const eventType = !eventDispatcher.isSuccess ? null : 
       eventTypes[eventDispatcher.data] || 'UNKNOWN';
 
@@ -73,11 +82,11 @@ export function TrustEvent({trustId, eventHash, ...rest}) {
           { eventDescription.isSuccess && <Text><b>{eventDescription.data}</b></Text> }
           { !eventDispatcher.isSuccess && <Skeleton width='15em' height='1em'/> }
           { eventType === 'KEY_ORACLE' && 
-            <KeyOracleEventDescription eventHash={eventHash}/> 
-          }
+            <KeyOracleEventDescription eventHash={eventHash}/> }
+          { eventType === 'ALARM_CLOCK' &&
+            <AlarmClockEventDescription eventHash={eventHash}/> }
           { eventType === 'UNKNOWN' &&
-            <Text color='gray'>We don't recognize the event dispatcher.</Text>
-          }
+            <Text color='gray'>We don't recognize the event dispatcher.</Text> }
         </VStack>
         <Spacer/>
       </HStack>
@@ -88,6 +97,12 @@ const KeyOracleEventDescription = ({eventHash, ...rest}) => {
   const eventKey = useEventKey(eventHash);
   return !eventKey.isSuccess ? <Skeleton width='12em' height='1em'/> :
     <KeyOracleKeyLabel keyId={eventKey.data}/> 
+}
+
+const AlarmClockEventDescription = ({eventHash, ...rest}) => {
+  const alarm = useAlarm(eventHash);
+  return !alarm.isSuccess ? <Skeleton width='12em' height='1em'/> :
+    <Text color='gray'>This alarm is set to go off by {(new Date(alarm.data.alarmTime.toNumber())).toString()}</Text>
 }
 
 const KeyOracleKeyLabel = ({keyId, ...rest}) => {
@@ -104,6 +119,7 @@ const KeyOracleKeyLabel = ({keyId, ...rest}) => {
 export const AddEventDialog = ({trustId, rootKeyId, isOpen, onClose}) => {
   const choicesDisclosure = useDisclosure();
   const keyOracleDisclosure = useDisclosure();
+  const alarmClockDisclosure = useDisclosure();
 
   return <Modal onClose={onClose} isOpen={isOpen} isCentered size='xl'>
       <ModalOverlay backdropFilter='blur(10px)'/>
@@ -113,7 +129,7 @@ export const AddEventDialog = ({trustId, rootKeyId, isOpen, onClose}) => {
         <ModalBody>
           <Collapse in={!choicesDisclosure.isOpen} width='100%'> 
           <Text>Choose an <b>event</b> you want to track as part of your trust's execution.</Text>
-          <List mt='1em' padding='1em'>
+          <List mt='1em' padding='1em' spacing='1em'>
             <ListItem>
               <HStack>
                 <Box width='5em'><FiPower color='green' size='40px'/></Box>
@@ -128,11 +144,29 @@ export const AddEventDialog = ({trustId, rootKeyId, isOpen, onClose}) => {
                 }}>Create</Button>
               </HStack>
             </ListItem>
+            <ListItem>
+              <HStack>
+                <Box width='5em'><BiAlarm color='red' size='40px'/></Box>
+                <VStack align='stretch' width='18em'>
+                  <Text fontWeight='bold'>Alarm Clock</Text>
+                  <Text>Create a snoozable alarm for a future date and time.</Text>
+                </VStack>
+                <Spacer/>
+                <Button colorScheme='blue' onClick={() => {
+                  choicesDisclosure.onOpen();
+                  alarmClockDisclosure.onOpen();
+                }}>Create</Button>
+              </HStack>
+            </ListItem>
           </List>
           </Collapse>
           <Collapse in={keyOracleDisclosure.isOpen} width='100%'>
             <KeyOracleForm trustId={trustId} rootKeyId={rootKeyId} onClose={onClose}
               onToggle={() => {keyOracleDisclosure.onToggle(); choicesDisclosure.onToggle();}}/>
+          </Collapse>
+          <Collapse in={alarmClockDisclosure.isOpen} width='100%'>
+            <AlarmClockForm trustId={trustId} rootKeyId={rootKeyId} onClose={onClose}
+              onToggle={() => {alarmClockDisclosure.onToggle(); choicesDisclosure.onToggle();}}/>
           </Collapse>
         </ModalBody>
         <ModalFooter>
@@ -212,4 +246,100 @@ export function TrustKeyOption({keyId, ...rest}) {
   const key = useInspectKey(keyId);
   return <option value={keyId.toString()}>{keyId.toString()}:&nbsp; 
     {key.isSuccess && key.data.alias}</option>
+}
+
+const AlarmClockForm = ({trustId, rootKeyId, onClose, onToggle}) => {
+  const keys = useTrustKeys(trustId);
+  const toast = useToast();
+
+  const [eventDescription, setEventDescription] = useState('');
+  const isDescriptionError = eventDescription.length < 5;
+  
+  const [snoozeKey, setSnoozeKey] = useState(null);
+  const [alarmTime, setAlarmTime] = useState(new Date((new Date().getTime()) + 86400000)); // 1 day future
+  const [snoozeNumber, setSnoozeNumber] = useState('0');
+  const [snoozeUnit, setSnoozeUnit] = useState(60*60*24);
+  const isSnoozeNumberError = snoozeNumber && snoozeNumber.length > 0 && isNaN(parseInt(snoozeNumber));
+  const isSnoozeKeyError = snoozeNumber && snoozeNumber.length > 0 
+    && !isSnoozeNumberError && parseInt(snoozeNumber) !== 0 && (snoozeKey === 'Choose Key' || !snoozeKey);
+
+  const createAlarmClock = useCreateAlarm(rootKeyId, isDescriptionError ? '' : eventDescription,
+    alarmTime.getTime(), (parseInt(snoozeNumber)||0)*(parseInt(snoozeUnit)||0), isSnoozeKeyError || !snoozeKey ? 0 : snoozeKey,
+    function(error) {
+      toast({
+        title: 'Transaction Error!',
+        description: error.toString(),
+        status: 'error',
+        duration: 9000,
+        isClosable: true
+      });
+    },
+    function(data) {
+      toast({
+        title: 'Alarm Clock event Created!',
+        description: 'They event has been added to your trust',
+        status: 'success',
+        duration: 9000,
+        isClosable: true
+      });
+      onClose();
+      onToggle();
+    }
+  );
+
+  const buttonProps = isSnoozeNumberError || isSnoozeKeyError || isDescriptionError ? {isDisabled: true} :
+    createAlarmClock.isLoading ? {isLoading: true} : {};
+
+  return <VStack spacing='2em'>
+    <FormControl id="Event Label" isInvalid={isDescriptionError}>
+      <FormLabel>Event Label</FormLabel>
+      <Input
+        placeholder="My ephemeral emersion."
+        _placeholder={{ color: 'gray.500' }}
+        onChange={(e) => { setEventDescription(e.target.value)}}/>
+      { isDescriptionError && <FormErrorMessage>Please provide a short description.</FormErrorMessage>}
+    </FormControl>
+    <FormControl id="Alarm Time">
+      <FormLabel>Alarm Date</FormLabel>
+      <DatePicker selected={alarmTime} onChange={(date) => setAlarmTime(date)} 
+        popperModifiers={[{
+          name: "preventOverflow",
+          options: {
+            altAxis: true,
+          }  
+        }]}/> 
+    </FormControl>
+    <FormControl id="Snooze Interval" isInvalid={isSnoozeNumberError||isSnoozeKeyError}>
+      <FormLabel>Snooze Interval and Key <i>(Optional)</i></FormLabel>
+      <HStack>
+        <Input
+          width='20%'
+          placeholder="0"
+          _placeholder={{ color: 'gray.500' }}
+          onChange={(e) => { setSnoozeNumber(e.target.value)}}/>
+        <Select variant='filled'
+          width='20%'
+          onChange={(e) => { setSnoozeUnit(e.target.value) }}>
+          <option value={60*60*24}>Days</option>    
+          <option value={60*60*24*7}>Weeks</option>    
+          <option value={60*60*24*365}>Years</option>    
+        </Select>
+        <Select width='55%' placeholder='Choose Key' variant='filled'
+          onChange={(e) => { setSnoozeKey(e.target.value) }}>
+          {keys.isSuccess &&
+            keys.data.map((k) => <TrustKeyOption key={'select-snooze-key' + k} keyId={k}/>)}
+        </Select>
+      </HStack>
+      { isSnoozeNumberError && <FormErrorMessage>The snooze interval must be a number.</FormErrorMessage>}
+      { isSnoozeKeyError && <FormErrorMessage>Non-zero snooze intervals require a valid trust key.</FormErrorMessage>}
+    </FormControl>
+    <HStack width='100%'>
+      <Spacer/>
+      <Button onClick={onToggle}>Cancel</Button>
+      <Button {...buttonProps} leftIcon={<HiOutlineLightningBolt/>} colorScheme='yellow'
+        onClick={
+          () => { createAlarmClock.write?.(); }
+        }>Mint Event</Button>
+    </HStack>
+  </VStack>
 }
