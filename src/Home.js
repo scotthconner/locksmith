f1a1516fb9388a791c07ab82c7d09b429d71e891
scotthconner <scotthconner@gmail.com>
2023-01-27 22:31:36 -0600

    Notes: tagline

--- src/Home.js
+++ src/Home.js
@@ -65,7 +65,7 @@ function Home() {
           <Heading fontSize={'5xl'}>cksmith</Heading>
         </HStack>
         <Text pb='1em' color={'gray.600'} fontSize={'xl'}>
-          Keep your wallet. Build your trust with Locksmith. 
+          Keep your wallet. Control it with Locksmith. 
         </Text>
         <HStack>
           <Button leftIcon={<IoIosDocument/>} colorScheme='gray' 
